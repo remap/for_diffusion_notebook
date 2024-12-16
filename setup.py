5af9589776ee09d255568b50718a1dfe2590d3b4
cboussema <57465196+cboussema@users.noreply.github.com>
2024-12-16 14:14:16 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import diffusers
 from diffusers import AutoPipelineForText2Image, AutoPipelineForImage2Image
 from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, LCMScheduler
+from diffusers import AutoPipelineForText2Image, DDIMScheduler, DPMSolverMultistepScheduler, EulerDiscreteScheduler
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler, AutoencoderKL, StableDiffusionXLControlNetPipeline
 from diffusers.utils import load_image, make_image_grid
+from diffusers.image_processor import IPAdapterMaskProcessor
+
+import cv2
 from google.colab.patches import cv2_imshow
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -10,10 +15,4 @@ import time
 from copy import deepcopy
 import os
 from pathlib import Path
-
-from diffusers.image_processor import IPAdapterMaskProcessor
-
-import cv2
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler, AutoencoderKL, StableDiffusionXLControlNetPipeline
-
 import numpy as np
